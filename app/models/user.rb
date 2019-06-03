b1c2f0fd2b8ed78428abf319b0a8448b6dbdaaca
mizuno <mizunoryota19950608@gmail.com>
2019-06-03 19:39:12 +0900

    Finish modeling

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
+
+  VALID_EMAIL_REJEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   has_secure_password
+
+  validates :name, presence: true, length: { maximum: 20 }
+  validates :email, presence: true, length: { maximum: 100 }, uniqueness: true,
+            format: { with: VALID_EMAIL_REJEX }
+  validates :password, presence: true, length: { minimum: 6 }
 end
